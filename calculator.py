95cc19fc3fc9adb2c9e6e287643f26cf7bd6f39b
Nisto23 <zanelesekgota@gmail.com>
2025-03-24 15:32:35 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,15 @@
+user = input ("enter an user (+ - * /)")
+Num1 = float(input("enter the 1st number:"))
+Num2 = float(input("enter the 2nd number:"))
+if user =="+":
+    result = Num1 + Num2
+    print(result)
+elif user =="-":
+     result = Num1 - Num2
+     print(result)
+elif user =="*":
+     result = Num1 * Num2 
+     print(result)   
+elif user =="/":
+     result = Num1 / Num2
+     print(result)  
