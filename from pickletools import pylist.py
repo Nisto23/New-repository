071c106f9e2a5648cf8f3d06b545baa904c99cc7
Nisto23 <zanelesekgota@gmail.com>
2025-04-01 12:49:37 +0200

    Add files via upload

---
+++ from pickletools import pylist.py
@@ -0,0 +1,17 @@
+from operator import indexOf
+from pickletools import pylist
+
+
+pylist
+
+mylist =[10, 20, 30, 40,]
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)
+mylist.append(40)
+mylist.insert(1,15) 
+mylist.extend([50,60,70])
+mylist.pop()
+mylist.sort()
+mylist.index(30)
+print("indexOf 30")
